5d9414f99cdb5d53ce0805d56deef5261a768a72
ankusena <147305874+ankusena@users.noreply.github.com>
2024-04-28 13:01:19 +0530

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,5 +1,5 @@
 <?php
-define ('url',"*6918983860:AAGp94A9inhJay50pL_OHf7TXpO0uchgS0w*");
+define ('https://manybot.io/webhook/6918983860/7fb05fd762',"*6918983860:AAGp94A9inhJay50pL_OHf7TXpO0uchgS0w*");
 
 $name = $_GET['name'];
 
@@ -11,6 +11,6 @@ $chat_id = '{1272046774}';
 
 $message = urlencode("Name:".$name."\nPhone: ".$phone."\nMessage: ".$message);
 
-file_get_contents(url."sendmessage?text=".$message."&chat_id=".$chat_id."&parse_mode=HTML");
+file_get_contents(https://manybot.io/webhook/6918983860/7fb05fd762."sendmessage?text=".$message."&chat_id=".$chat_id."&parse_mode=HTML");
 
 ?>
