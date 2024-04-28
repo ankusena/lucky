1060694e1ca89266525a080b937352a2a32d02cc
ankusena <147305874+ankusena@users.noreply.github.com>
2024-04-28 09:10:13 +0000

    changes

--- submit.php
+++ submit.php
@@ -1,28 +1,16 @@
 <?php
-// Define the base URL for the Telegram Bot API
-define ('url', "https://api.telegram.org/bot6918983860:AAGp94A9inhJay50pL_OHf7TXpO0uchgS0w/");
+define ('url',"https://api.telegram.org/bot6918983860:AAGp94A9inhJay50pL_OHf7TXpO0uchgS0w/getUpdates");
 
-// Extract values from the URL parameters
 $name = $_GET['name'];
+
 $message = $_GET['message'];
+
 $phone = $_GET['phone'];
 
-// Define the chat ID
 $chat_id = '-1002018391847';
 
-// Construct the message
-$message_text = urlencode("Name: $name\nPhone: $phone\nMessage: $message");
+$message = urlencode("Name:".$name."\nPhone: ".$phone."\nMessage: ".$message);
 
-// Construct the URL for sending the message
-$url = url . "sendMessage?text=$message_text&chat_id=$chat_id&parse_mode=HTML";
+file_get_contents(url."sendmessage?text=".$message."&chat_id=".$chat_id."&parse_mode=HTML");
 
-// Send the message using file_get_contents
-$response = file_get_contents($url);
-
-// Check if the message was sent successfully
-if ($response) {
-    echo "Message sent successfully!";
-} else {
-    echo "Error sending message.";
-}
 ?>
